7769372b85b5659c5dbcc94b9a45280601677213
wildmaru <wildmaru@gmail.com>
2019-05-08 14:43:18 -0400

    selection sort

--- basic/SelectionSort.cpp
+++ basic/SelectionSort.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+
+int main(void) {
+  int i, j, min, idx, tmp;
+  int array[8] = {5,4,8,7,6,3,2,1};
+
+  for(i=0; i<8; i++) {
+    min = 100;
+    for(j=i; j<8; j++) {
+      if(min > array[j]) {
+        min = array[j];
+        idx = j;
+      }
+    }
+
+    tmp = array[i];
+    array[i] = array[idx];
+    array[idx] = tmp;
+  }
+
+  for(i=0; i<8; i++) {
+    printf("%d", array[i]);
+  }
+
+  return 0;
+}
